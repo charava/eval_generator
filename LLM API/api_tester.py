d643906bc2780162295fa358ae221f0f959cd0da
charava <charosa@nuevaschool.org>
2023-10-17 22:51:59 -0700

    fix

--- LLM API/api_tester.py
+++ LLM API/api_tester.py
@@ -6,8 +6,6 @@ import requests
 # run app.py
 # then run api_tester.py in a seperate terminal
 
-
-
 url = 'http://127.0.0.1:5000/post'
 params = {
     'prompt': 'please rephrase the following: it was an absolute pleasure teaching you. I was proud of you and your classmates for creating a close-knit, open, and safe environment to explore sleep science, drug education, sex education, and various perspectives of the Nueva experience.'
